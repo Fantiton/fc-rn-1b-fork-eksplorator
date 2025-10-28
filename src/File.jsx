b2a8913dc720acf08150d23422cddff9b58a7195
FC D208 TEB <fc@teb.edu.pl>
2025-10-28 11:56:36 +0100

    + Ikony + decentralizacja cssa + rozwijanie folderów

--- src/File.jsx
+++ src/File.jsx
@@ -4,6 +4,7 @@ function File({ file }) {
     return (
         <>
             <div className="file">
+                <img className='icon' src='../src/images/iconFile.png'></img>
                 <h3>{ file.name }</h3>
                 <p>{ file.type }</p>
             </div>
--- src/Folder.jsx
+++ src/Folder.jsx
@@ -1,34 +1,64 @@
 import File from './File.jsx'
+import React from 'react'
+import "./Folder.css"
 
 function Folder({ data }) {
+    const [state, setState] = React.useState('closed')
+
     function display(item) {
         return item.children.map((item) => 
-            {
-                if (item.type === "folder") {
-                    return (
-                        <>
-                            {<Folder data={item} />}
-                        </>
-                    )
-                } else {
-                    return (
-                        <>
-                            {<File file={item} />}
-                        </>
-                    )
-                }
+        {
+            if (item.type === "folder") {
+                return (
+                    <>
+                        {<Folder data={item} />}
+                    </>
+                )
+            } else {
+                return (
+                    <>
+                        {<File file={item} />}
+                    </>
+                )
             }
-        )
+        })
+    }
+    function returnFolder(){
+        if(state == 'open'){
+            return (
+                <>
+                    <div className="folder" >
+                        <div onClick={OpenFolder}>
+                            <img className='icon' src='../src/images/iconFolderOpen.png'></img>
+                            <h3>{ data.name }</h3>
+                            <p>{ data.type }</p>
+                        </div>
+                        {display(data)}
+                    </div>
+                </>
+            )
+        } else{
+            return(
+                <>
+                    <div className='folder' onClick={OpenFolder}>
+                        <img className='icon' src='../src/images/iconFolderClosed.png' ></img>
+                        <h3>{ data.name }</h3>
+                    </div>
+                </>
+            )
+        }
+    }
+
+    function OpenFolder(){
+        if(state == 'open'){
+            setState('closed')
+        } else{
+            setState('open')
+        }
     }
 
     return (
-        <>
-            <div className="folder">
-                <h3>{ data.name }</h3>
-                <p>{ data.type }</p>
-                {display(data)}
-            </div>
-        </>
+        returnFolder()
     )
 }
 
